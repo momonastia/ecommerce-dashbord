058eee33412bbc9b8ef769f4078199b57bbb392c
momonastia~ <anastasiatyrikova@gmail.com>
2023-01-23 17:43:57 +0100

    esercitazione 23-01-23

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Container from "./components/container/Container";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
