22cb2f165ab962ab58dc155fc8fdd9887bc13ef7
momonastia~ <anastasiatyrikova@gmail.com>
2023-03-02 10:02:30 +0100

    Fix: charts responsive

--- src/constants/chartsData.js
+++ src/constants/chartsData.js
@@ -1,45 +1,45 @@
 const data = [
   {
     name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    uv: 400,
+    pv: 240,
+    amt: 240,
   },
   {
     name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    uv: 300,
+    pv: 139,
+    amt: 221,
   },
   {
     name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    uv: 200,
+    pv: 980,
+    amt: 229,
   },
   {
     name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    uv: 278,
+    pv: 390,
+    amt: 200,
   },
   {
     name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    uv: 189,
+    pv: 480,
+    amt: 218,
   },
   {
     name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    uv: 239,
+    pv: 380,
+    amt: 250,
   },
   {
     name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    uv: 349,
+    pv: 430,
+    amt: 210,
   },
 ];
 
@@ -72,11 +72,11 @@ const data01 = [
 const data02 = [
   {
     name: "Group A",
-    value: 2400,
+    value: 240,
   },
   {
     name: "Group B",
-    value: 4567,
+    value: 456,
   },
   {
     name: "Group C",
@@ -84,7 +84,7 @@ const data02 = [
   },
   {
     name: "Group D",
-    value: 9800,
+    value: 980,
   },
   {
     name: "Group E",
@@ -92,7 +92,7 @@ const data02 = [
   },
   {
     name: "Group F",
-    value: 4800,
+    value: 480,
   },
 ];
 
