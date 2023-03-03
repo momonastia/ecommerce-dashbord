befb7aa444e55752e73226be499cc9ee1fb5b7a2
momonastia~ <anastasiatyrikova@gmail.com>
2023-03-03 18:07:44 +0100

    Fixed: responsive all home charts

--- src/components/salesBarReport/SalesBarReport.jsx
+++ src/components/salesBarReport/SalesBarReport.jsx
@@ -6,6 +6,7 @@ import {
   CartesianGrid,
   Legend,
   Bar,
+  ResponsiveContainer,
 } from "recharts";
 import { data03 } from "../../constants/chartsData.js";
 import styles from "./styles.module.scss";
@@ -14,15 +15,17 @@ const SalesBarReport = ({ children }) => {
   return (
     <div className={styles.mainSalesBarReport}>
       <h4>{children}</h4>
-      <BarChart width={280} height={120} data={data03}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" fill="#f15b5d" />
-        <Bar dataKey="uv" fill="#0ebe75" />
-      </BarChart>
+      <ResponsiveContainer width="100%">
+        <BarChart width={300} height={120} data={data03}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#f15b5d" />
+          <Bar dataKey="uv" fill="#0ebe75" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
